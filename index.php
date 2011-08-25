3facf0f39fdddbfc38b925d61fcfb7be061993f7
Sebastian Tramp <mail@sebastian.tramp.name>
2011-08-25 15:11:26 +0200

    + mimetype and parameter check

--- index.php
+++ index.php
@@ -3,9 +3,13 @@ header('Access-Control-Allow-Origin: *');
 include_once("phplibs/graphite/arc/ARC2.php");
 include_once("phplibs/graphite/Graphite.php");
 
-$url = $_REQUEST['url'];
-
-$graph = new Graphite();
-$graph->load( $url );
-print $graph->serialize( "RDFJSON" );
+if (isset($_REQUEST['url'])){
+    $url = $_REQUEST['url'];
+    $graph = new Graphite();
+    $graph->load( $url );
+    header('Content-type: application/json');
+    print $graph->serialize( "RDFJSON" );
+} else {
+    print "no url parameter, so I do not know where to fetch the rdf data from.";
+}
 
